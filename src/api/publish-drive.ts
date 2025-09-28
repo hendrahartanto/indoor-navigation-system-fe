8871d3a2b44bb989050d67cfb09266d022fbdbfe
Andrew <92193378+Andrew7-7@users.noreply.github.com>
2025-09-28 13:19:46 +0700

    fix: Fix url

--- src/api/publish-drive.ts
+++ src/api/publish-drive.ts
@@ -3,6 +3,9 @@ import axios from "axios";
 export const publishDriveCommand = async (key: string, type: string) => {
   const enable = type === "down" ? true : false;
   console.log("Publishing drive command:", { direction: key, enable });
-  const res = await axios.post("/motor/drive", { direction: key, enable });
+  const res = await axios.post(
+    `${import.meta.env.VITE_BACKEND_URL}/motor/drive`,
+    { direction: key, enable }
+  );
   return res.data;
 };
